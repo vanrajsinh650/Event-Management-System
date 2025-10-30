fca1312305a5cb1ce74070fe55e7cdb8e3551ac6
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 15:56:22 +0530

    add event model

---
+++ events/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organized_events')
+    location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
