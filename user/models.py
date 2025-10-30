5d02d5133f0dab4a25e6a7310c2a3a8cf0097d3c
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 15:05:00 +0530

    add basic UserProfile model

---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
