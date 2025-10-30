b84c52413411b8c2949c1b65a669c4bf10d83f48
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 17:00:05 +0530

    add review serializer with validation

--- events/serializers.py
+++ events/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Event
 from .models import Event, RSVP
+from .models import Event, RSVP, Review
 
 class EventSerializer(serializers.ModelSerializer):
     organizer_name = serializers.CharField(source='organizer.username', read_only=True)
@@ -43,6 +44,25 @@ class RSVPSerializer(serializers.ModelSerializer):
                 'error': 'Event organizer cannot RSVP to their own event.'
             })
         return attrs
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        validated_data['event'] = self.context['event']
+        return super().create(validated_data)
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.username', read_only=True)
+    event_title = serializers.CharField(source='event.title', read_only=True)
+    
+    class Meta:
+        model = Review
+        fields = ['id', 'event', 'user', 'user_name', 'event_title', 'rating', 'comment', 'created_at']
+        read_only_fields = ['user', 'event', 'created_at']
+    
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError('Rating must be between 1 and 5.')
+        return value
     
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
