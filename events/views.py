854e7c8e4814140a424b3204b7e7620793d7f1ba
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 16:41:05 +0530

    add RSVP creation endpoint0

--- events/views.py
+++ events/views.py
@@ -1,8 +1,11 @@
+from urllib import response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 from .models import Event
 from .serializers import EventSerializer
 from .permissions import IsOrganizerOrReadOnly
+from .models import Event, RSVP
+from .serializers import EventSerializer, RSVPSerializer
 
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.filter(is_public=True)
@@ -27,3 +30,17 @@ class EventDeleteView(generics.DestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticated, IsOrganizerOrReadOnly]
+
+class RSVPCreateView(generics.CreateAPIView):
+    serializer_class = RSVPSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, event_id):
+        try:
+            event = Event.objects.get(id=event_id)
+        except Event.DoesNotExist:
+            return response({'error': 'Event not found.'}, status=404)
+        
+        serializer = self.get_serializer(data=request.data, context={'request': request, 'event': event})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
