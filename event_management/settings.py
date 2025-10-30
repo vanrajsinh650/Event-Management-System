fbb078804881ac82d01f74504a45f5085aa894b6
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 17:22:22 +0530

    add Celery and email configuration

--- event_management/settings.py
+++ event_management/settings.py
@@ -3,3 +3,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+#Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
+#Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'noreply@eventmanagement.com'
