c85aa29d449efb52ef687dc1a1995527923422ed
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 16:04:03 +0530

    add event list and create endpoints

---
+++ events/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import Event
+from .serializers import EventSerializer
+
+class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [AllowAny]
