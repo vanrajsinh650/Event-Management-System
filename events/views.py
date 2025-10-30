bd4519fe490b7afc64fbd249769ed0e285741be6
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 16:07:02 +0530

    fix require authentication for event creation

--- events/views.py
+++ events/views.py
@@ -1,9 +1,18 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 from .models import Event
 from .serializers import EventSerializer
 
 class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.filter(is_public=True)
+    serializer_class = EventSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+class EventDetailView(generics.RetrieveAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [AllowAny]
