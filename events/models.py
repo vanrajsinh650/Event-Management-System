1350c1f31f56228a7e2a6a947e5b83c5ab60f0df
vanrajsinh650 <vanrajsinhsolanki650@gmail.com>
2025-10-30 15:57:34 +0530

    add time validation and public/private events

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -8,7 +9,21 @@ class Event(models.Model):
     location = models.CharField(max_length=255)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    is_public = models.BooleanField(default=True)  # Added this
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # Added this
+    
+    class Meta:
+        ordering = ['-start_time']
     
     def __str__(self):
         return self.title
+    
+    def clean(self):
+        if self.start_time and self.end_time:
+            if self.end_time <= self.start_time:
+                raise ValidationError({'end_time': 'End time must be after start time.'})
+    
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
